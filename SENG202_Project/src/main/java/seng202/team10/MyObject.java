2c00d2ca630e5068fbc5bbe3ea90fc8ad71a0def
Andy Holden <ash102@uclive.ac.nz>
2018-08-08 14:15:32 +1200

    Added Java Files

---
+++ SENG202_Project/src/main/java/seng202/team10/MyObject.java
@@ -0,0 +1,16 @@
+package seng202.team10;
+
+/**
+ * MyObject
+ * 
+ * @author ash102
+ */
+public class MyObject {
+		
+	private String name = "The name of my object";
+	private int quantity = 11;
+	
+	MyObject() {
+		// Dummy Constructor
+	}
+}
