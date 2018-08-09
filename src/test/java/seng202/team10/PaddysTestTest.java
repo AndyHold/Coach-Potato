d5e168f0663753da03a3e152ea89983225045bb8
Paddy <pmm118@uclive.ac.nz>
2018-08-10 10:36:10 +1200

    Paddy's test
    Paddy's test
    
    Paddy's test
    
    Paddy's test
    
    Paddy's test
    
    Paddy's test
    
    Paddy's test

--- src/test/java/seng202/team10/PaddysTestTest.java
+++ src/test/java/seng202/team10/PaddysTestTest.java
@@ -34,6 +34,30 @@ public class PaddysTestTest
     public void testApp()
     {
         PaddysTest paddysTest = new PaddysTest();
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        assertEquals( paddysTest.getName(), "Hi my name is Paddy" );
+=======
         assertEquals( andysTest.getName(), "Hi my name is Paddy" );
+>>>>>>> 026b07c... Paddy's test
+=======
+        assertEquals( paddysTest.getName(), "Hi my name is Paddy" );
+>>>>>>> 85c2a6c... Paddy's test
+=======
+        assertEquals( andysTest.getName(), "Hi my name is Paddy" );
+>>>>>>> cf0a651... Paddy's test
+=======
+        assertEquals( paddysTest.getName(), "Hi my name is Paddy" );
+>>>>>>> bdfbd06... Paddy's test
+=======
+        assertEquals( andysTest.getName(), "Hi my name is Paddy" );
+>>>>>>> c2f3f52... Paddy's test
+=======
+        assertEquals( paddysTest.getName(), "Hi my name is Paddy" );
+>>>>>>> c5bb697... Paddy's test
     }
 }
