baaef1b9c05eacd2e035daef4542586b3525497b
ash102 <ash102@uclive.ac.nz>
2018-08-22 13:03:51 +1200

    Added Java Doc for each Class
    Added Java Doc for each Class
    
    Added Java Doc for each Class
    
    Added Java Doc for each Class

--- src/main/java/seng202/team10/Model/ActivitiesData/Activities.java
+++ src/main/java/seng202/team10/Model/ActivitiesData/Activities.java
@@ -1,4 +1,11 @@
 package seng202.team10.Model.ActivitiesData;
 
+
+
+/**
+ * Activity Class for Coach Potato
+ * SENG202 2018S2
+ * @author Andrew Holden, Cam Arnold, Paddy Mitchell, Priyesh Shah, Torben Klausen
+ */
 public class Activities {
 }
