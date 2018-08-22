4453dbc7bc2cca7fb3095b2d7317f80e13f971b2
Paddy <pmm118@uclive.ac.nz>
2018-08-23 08:52:24 +1200

    Implementation of UserProfile class getter and setter methods
    Implementation of UserProfile class getter and setter methods
    
    Implementation of UserProfile class getter and setter methods
    
    Implementation of UserProfile class getter and setter methods
    
    Implementation of UserProfile class getter and setter methods
    
    Implementation of UserProfile class getter and setter methods
    
    Implementation of UserProfile class getter and setter methods

--- src/main/java/seng202/team10/Model/UserProfile.java
+++ src/main/java/seng202/team10/Model/UserProfile.java
@@ -27,49 +27,273 @@ public class UserProfile {
      * Getter method for the name of the user
      * @return String
      */
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public String getName() {
+        return this.name;
+    }
+=======
     public String getName() { return this.name; }
+>>>>>>> 7deda61... Implementation of UserProfile class getter and setter methods
+=======
+    public String getName() {
+        return this.name;
+    }
+>>>>>>> eff27ce... Implementation of UserProfile class getter and setter methods
+=======
+    public String getName() { return this.name; }
+>>>>>>> 90c2806... Implementation of UserProfile class getter and setter methods
+=======
+    public String getName() {
+        return this.name;
+    }
+>>>>>>> a85d43e... Implementation of UserProfile class getter and setter methods
+=======
+    public String getName() { return this.name; }
+>>>>>>> 35a851d... Implementation of UserProfile class getter and setter methods
+=======
+    public String getName() {
+        return this.name;
+    }
+>>>>>>> a40329b... Implementation of UserProfile class getter and setter methods
 
 
     /**
      * Setter method for the name of the user
      * @param newName: String
      */
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public void setName(String newName) {
+        this.name = newName;
+    }
+=======
     public void setName(String newName) { this.name = newName; }
+>>>>>>> 7deda61... Implementation of UserProfile class getter and setter methods
+=======
+    public void setName(String newName) {
+        this.name = newName;
+    }
+>>>>>>> eff27ce... Implementation of UserProfile class getter and setter methods
+=======
+    public void setName(String newName) { this.name = newName; }
+>>>>>>> 90c2806... Implementation of UserProfile class getter and setter methods
+=======
+    public void setName(String newName) {
+        this.name = newName;
+    }
+>>>>>>> a85d43e... Implementation of UserProfile class getter and setter methods
+=======
+    public void setName(String newName) { this.name = newName; }
+>>>>>>> 35a851d... Implementation of UserProfile class getter and setter methods
+=======
+    public void setName(String newName) {
+        this.name = newName;
+    }
+>>>>>>> a40329b... Implementation of UserProfile class getter and setter methods
 
 
     /**
      * Getter method for the activities of the user
      * @return Activities
      */
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public Activities getActivities() {
+        return this.activities;
+    }
+=======
     public Activities getActivities() { return this.activities; }
+>>>>>>> 7deda61... Implementation of UserProfile class getter and setter methods
+=======
+    public Activities getActivities() {
+        return this.activities;
+    }
+>>>>>>> eff27ce... Implementation of UserProfile class getter and setter methods
+=======
+    public Activities getActivities() { return this.activities; }
+>>>>>>> 90c2806... Implementation of UserProfile class getter and setter methods
+=======
+    public Activities getActivities() {
+        return this.activities;
+    }
+>>>>>>> a85d43e... Implementation of UserProfile class getter and setter methods
+=======
+    public Activities getActivities() { return this.activities; }
+>>>>>>> 35a851d... Implementation of UserProfile class getter and setter methods
+=======
+    public Activities getActivities() {
+        return this.activities;
+    }
+>>>>>>> a40329b... Implementation of UserProfile class getter and setter methods
 
 
     /**
      * Getter method for the weight of the user
      * @return double
      */
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public double getWeight() {
+        return this.weight;
+    }
+=======
     public double getWeight() { return this.weight; }
+>>>>>>> 7deda61... Implementation of UserProfile class getter and setter methods
+=======
+    public double getWeight() {
+        return this.weight;
+    }
+>>>>>>> eff27ce... Implementation of UserProfile class getter and setter methods
+=======
+    public double getWeight() { return this.weight; }
+>>>>>>> 90c2806... Implementation of UserProfile class getter and setter methods
+=======
+    public double getWeight() {
+        return this.weight;
+    }
+>>>>>>> a85d43e... Implementation of UserProfile class getter and setter methods
+=======
+    public double getWeight() { return this.weight; }
+>>>>>>> 35a851d... Implementation of UserProfile class getter and setter methods
+=======
+    public double getWeight() {
+        return this.weight;
+    }
+>>>>>>> a40329b... Implementation of UserProfile class getter and setter methods
 
 
     /**
      * Getter method for the height of the user
      * @return double
      */
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public double getHeight() {
+        return this.height;
+    }
+=======
     public double getHeight() { return this.height; }
+>>>>>>> 7deda61... Implementation of UserProfile class getter and setter methods
+=======
+    public double getHeight() {
+        return this.height;
+    }
+>>>>>>> eff27ce... Implementation of UserProfile class getter and setter methods
+=======
+    public double getHeight() { return this.height; }
+>>>>>>> 90c2806... Implementation of UserProfile class getter and setter methods
+=======
+    public double getHeight() {
+        return this.height;
+    }
+>>>>>>> a85d43e... Implementation of UserProfile class getter and setter methods
+=======
+    public double getHeight() { return this.height; }
+>>>>>>> 35a851d... Implementation of UserProfile class getter and setter methods
+=======
+    public double getHeight() {
+        return this.height;
+    }
+>>>>>>> a40329b... Implementation of UserProfile class getter and setter methods
 
 
     /**
      * Setter method for the weight of the user
      * @param newWeight
      */
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+=======
     public void setWeight(double newWeight) { this.weight = newWeight; }
+>>>>>>> 7deda61... Implementation of UserProfile class getter and setter methods
+=======
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+>>>>>>> eff27ce... Implementation of UserProfile class getter and setter methods
+=======
+    public void setWeight(double newWeight) { this.weight = newWeight; }
+>>>>>>> 90c2806... Implementation of UserProfile class getter and setter methods
+=======
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+>>>>>>> a85d43e... Implementation of UserProfile class getter and setter methods
+=======
+    public void setWeight(double newWeight) { this.weight = newWeight; }
+>>>>>>> 35a851d... Implementation of UserProfile class getter and setter methods
+=======
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+>>>>>>> a40329b... Implementation of UserProfile class getter and setter methods
 
 
     /**
      * Setter method for the height of the user
      * @param newHeight
      */
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public void setHeight(double newHeight) {
+        this.height = newHeight;
+    }
+=======
     public void setHeight(double newHeight) { this.height = newHeight; }
+>>>>>>> 7deda61... Implementation of UserProfile class getter and setter methods
+=======
+    public void setHeight(double newHeight) {
+        this.height = newHeight;
+    }
+>>>>>>> eff27ce... Implementation of UserProfile class getter and setter methods
+=======
+    public void setHeight(double newHeight) { this.height = newHeight; }
+>>>>>>> 90c2806... Implementation of UserProfile class getter and setter methods
+=======
+    public void setHeight(double newHeight) {
+        this.height = newHeight;
+    }
+>>>>>>> a85d43e... Implementation of UserProfile class getter and setter methods
+=======
+    public void setHeight(double newHeight) { this.height = newHeight; }
+>>>>>>> 35a851d... Implementation of UserProfile class getter and setter methods
+=======
+    public void setHeight(double newHeight) {
+        this.height = newHeight;
+    }
+>>>>>>> a40329b... Implementation of UserProfile class getter and setter methods
 
 
     /**
