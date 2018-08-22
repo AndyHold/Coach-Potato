114f8a8f8125346919c5a38be77b148fce9526cd
ash102 <ash102@uclive.ac.nz>
2018-08-22 13:03:51 +1200

    Added Java Doc for each Class

--- src/main/java/seng202/team10/Model/GoalType.java
+++ src/main/java/seng202/team10/Model/GoalType.java
@@ -1,4 +1,11 @@
 package seng202.team10.Model;
 
+
+
+/**
+ * GoalType Class for Coach Potato
+ * SENG202 2018S2
+ * @author Andrew Holden, Cam Arnold, Paddy Mitchell, Priyesh Shah, Torben Klausen
+ */
 public enum GoalType {
 }
