17e35ec3c7a29228e19063f93b10ad6dd50b24a9
Priyesh <psh76@uclive.ac.nz>
2018-08-23 09:53:18 +1200

    Made getters and setters for Entry and Position classes in ActivitiesData package in Model.

--- src/main/java/seng202/team10/Model/ActivitiesData/Position.java
+++ src/main/java/seng202/team10/Model/ActivitiesData/Position.java
@@ -8,4 +8,66 @@ package seng202.team10.Model.ActivitiesData;
  * @author Andrew Holden, Cam Arnold, Paddy Mitchell, Priyesh Shah, Torben Klausen
  */
 public class Position {
+
+    private double latitude;
+    private double longitude;
+    private double elevation;
+
+
+    /**
+     * Getter method to get user's latitude.
+     * @return double
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Setter method to set user's distance.
+     * @param latitude: double
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * Getter method to get user's longitude.
+     * @return double
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Setter method to set user's longitude.
+     * @param longitude: double
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * Getter method to get user's elevation.
+     * @return double
+     */
+    public double getElevation() {
+        return elevation;
+    }
+
+    /**
+     * Setter method to set user's elevation.
+     * @param elevation: double
+     */
+    public void setElevation(double elevation) {
+        this.elevation = elevation;
+    }
+
+    /**
+     * Setter method to set user's elevation.
+     * @param position: double
+     * @return double
+     */
+    public double subtract(Position position) {
+        return latitude; // Check with the team about this
+    }
 }
