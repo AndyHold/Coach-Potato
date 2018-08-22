3c314392f66e8a2ae80c0a74b01cc9579eb4024b
Cam Arnold <cta67@uclive.ac.nz>
2018-08-23 10:30:31 +1200

    some file write framework
    some file write framework
    
    some file write framework
    
    some file write framework

--- src/main/java/seng202/team10/Model/FileOperations/FileWriter.java
+++ src/main/java/seng202/team10/Model/FileOperations/FileWriter.java
@@ -1,6 +1,31 @@
 package seng202.team10.Model.FileOperations;
 
+import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.UserProfile;
+
 public class FileWriter {
+
+    private UserProfile activeProfile;
+
+    public void saveProfile(UserProfile profile){
+        activeProfile = profile;
+        if(checkProfileFolderExists(activeProfile) == false){
+            createProfileFolder(activeProfile);
+        }
+    }
+
+    public void saveActivity(Activity activity){
+
+    }
+
+    public void createProfileFolder(UserProfile profile){
+
+    }
+
+    private boolean checkProfileFolderExists(UserProfile profile) {
+
+        return exists;
+    }
 
 
 }
