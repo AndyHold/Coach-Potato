b3884b6ea094198504a693be37343a95051a13b4
Priyesh <psh76@uclive.ac.nz>
2018-08-29 15:57:45 +1200

    No changes made

--- src/main/java/seng202/team10/Model/ActivitiesData/Entry.java
+++ src/main/java/seng202/team10/Model/ActivitiesData/Entry.java
@@ -10,27 +10,35 @@ package seng202.team10.Model.ActivitiesData;
 public class Entry {
 
     private boolean firstEntry;
-    private double time;
+    private DateTime time;
     private double heartRate;
     private Position position;
     private double distance = 0;
     private double velocity = 0;
-    private Activity activity;
+    //private Activity activity;
 
     /**
      * Constructor for the Entry.
+<<<<<<< HEAD
      * @param firstEntry True if first entry.
      * @param time Time field of the entry.
      * @param heartRate Heart rate field of the entry.
      * @param position Position of the entry.
      * @param activity Activity field of the entry.
+=======
+     * @param firstEntry: boolean True if first entry.
+     * @param time: double Time field of the entry.
+     * @param heartRate: double Heart rate field of the entry.
+     * @param position: Position Position of the entry.
+     * //@param activity: Activity Activity field of the entry.
+>>>>>>> f5029c7d... No changes made
      */
-    public Entry(boolean firstEntry, double time, double heartRate, Position position, Activity activity) {
+    public Entry(boolean firstEntry, DateTime time, double heartRate, Position position) {
         this.firstEntry = firstEntry;
         this.time = time;
         this.heartRate = heartRate;
         this.position = position;
-        this.activity = activity;
+        //this.activity = activity;
     }
 
     /**
@@ -53,7 +61,7 @@ public class Entry {
      * Getter method to get user's entry time.
      * @return double
      */
-    public double getTime() {
+    public DateTime getTime() {
         return time;
     }
 
@@ -61,7 +69,7 @@ public class Entry {
      * Setter method to set user's entry time.
      * @param time: double
      */
-    public void setTime(double time) {
+    public void setTime(DateTime time) {
         this.time = time;
     }
 
@@ -128,17 +136,18 @@ public class Entry {
      */
     public void calculateVelocity(Entry prevEntry) {
         double prevDistance = prevEntry.getDistance();
-        double prevTime = prevEntry.getTime();
-        this.velocity = (this.distance - prevDistance)/(this.time-prevTime);
+        DateTime prevTime = prevEntry.getTime();
+        this.velocity = (this.distance - prevDistance)/((time.getMinute()-prevTime.getMinute())*60);
     }
 
     /**
      * Getter method to get user's activities.
      * @return  activities: Activities
      */
-    public Activity getActivity() {
-        return activity;
-    }
+    // Commented out as we don't need it for now but might need it later.
+    //public Activity getActivity() {
+    //    return activity;
+    //}
 
 
 
---
+++ src/test/java/seng202/team10/Model/ActivitiesData/PositionTest.java
@@ -0,0 +1,25 @@
+package seng202.team10.Model.ActivitiesData;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PositionTest {
+
+    @Test
+    public void subtract() {
+        Position position1 = new Position(0,0,0);
+        Position position2 = new Position(1,2,3);
+        assertTrue(position2.subtract(position1) == Math.sqrt(14));
+
+    }
+
+    @Test
+    public void negSubtract() {
+
+        Position position3 = new Position(-1,-2,-3);
+        Position position4 = new Position(0,0,0);
+        assertTrue(position4.subtract(position3) == Math.sqrt(14));
+    }
+
+}
