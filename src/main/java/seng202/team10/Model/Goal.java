bcb6280ea7104bbcdc19d52c2ba9423c2076c99d
Paddy <pmm118@uclive.ac.nz>
2018-08-29 12:55:54 +1200

    Began implementation of Goal classes. The types of goals extend the Goal class (inheritance). Added constructor and getters to goal class and the createGoal(), addGoal(), removeGoal() and checkGoals() methods to the Goals class.

--- src/main/java/seng202/team10/Model/Goal.java
+++ src/main/java/seng202/team10/Model/Goal.java
@@ -1,11 +1,66 @@
 package seng202.team10.Model;
 
 
-
-/**
- * Goal Class for Coach Potato
- * SENG202 2018S2
- * @author Andrew Holden, Cam Arnold, Paddy Mitchell, Priyesh Shah, Torben Klausen
- */
 public class Goal {
+
+    private int goalStartDate;
+    private int goalTargetDate;
+    private String goalType;
+    private Boolean goalAchievedStatus;
+    private double goalVelocity;
+    private int goalFrequency;
+    private double goalTime;
+    private double goalWeight;
+    private double goalDistance;
+
+
+    public Goal(int startDate, int targetDate, String type, Boolean achieved, double velocity, int frequency, double time, double weight, double distance) {
+        goalStartDate = startDate;
+        goalTargetDate = targetDate;
+        goalType = type;
+        goalAchievedStatus = achieved;
+        goalVelocity = velocity;
+        goalFrequency = frequency;
+        goalTime = time;
+        goalWeight = weight;
+        goalDistance = distance;
+
+    }
+
+    public int getGoalStartDate() {
+        return this.goalStartDate;
+    }
+
+    public int getGoalTargetDate() {
+        return this.goalTargetDate;
+    }
+
+    public String getGoalType() {
+        return goalType;
+    }
+
+    public Boolean getGoalAchieved() {
+        return goalAchievedStatus;
+    }
+
+    public double getGoalVelocity() {
+        return goalVelocity;
+    }
+
+    public double getGoalDistance() {
+        return goalDistance;
+    }
+
+    public double getGoalTime() {
+        return goalTime;
+    }
+
+    public double getGoalWeight() {
+        return goalWeight;
+    }
+
+    public int getGoalFrequency() {
+        return goalFrequency;
+    }
+
 }
