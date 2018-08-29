6c217be9ea506790a3f6a3f1db56bb628fe4cf98
Priyesh <psh76@uclive.ac.nz>
2018-08-29 12:55:37 +1200

    Implemented MainController class with basic attributes and  methods. Also created an addActivity method to UserProfile class.

--- src/main/java/seng202/team10/Control/MainController.java
+++ src/main/java/seng202/team10/Control/MainController.java
@@ -1,4 +1,41 @@
 package seng202.team10.Control;
 
+import seng202.team10.Model.FileOperations.FileReader;
+import seng202.team10.Model.FileOperations.FileWriter;
+import seng202.team10.Model.FileOperations.Parser;
+import seng202.team10.Model.UserProfile;
+
+import java.util.ArrayList;
+
 public class MainController {
+
+    private static ArrayList<UserProfile> users;
+    private static UserProfile currentUser;
+    private static Parser parser;
+    private static GUIController guiController;
+    private static FileWriter dataWriter = new FileWriter();
+    private static FileReader dataReader = new FileReader();
+
+    public void openLoginScreen() {
+        //guiController.openLoginScreen(guiController);
+    }
+
+    public void saveData() {
+        dataWriter.saveProfile(currentUser);
+    }
+
+    public void loadData() {
+        dataReader.loadExistingProfile(currentUser.getName());
+    }
+
+    public static void main(String[] args) {
+        //check if profiles folder exist, if not create a new profile
+        //Check which profiles exists, update users list
+        //users = dataReader.getExistingUsers();
+
+    }
+
+    public UserProfile getCurrentUser() {
+        return currentUser;
+    }
 }
