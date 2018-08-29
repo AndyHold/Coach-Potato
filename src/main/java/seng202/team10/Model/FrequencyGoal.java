10d3f5567eea94356c7d1898cc0905edddb82aab
Paddy <pmm118@uclive.ac.nz>
2018-08-29 12:55:54 +1200

    Began implementation of Goal classes. The types of goals extend the Goal class (inheritance). Added constructor and getters to goal class and the createGoal(), addGoal(), removeGoal() and checkGoals() methods to the Goals class.
    Began implementation of Goal classes. The types of goals extend the Goal class (inheritance). Added constructor and getters to goal class and the createGoal(), addGoal(), removeGoal() and checkGoals() methods to the Goals class.
    
    Began implementation of Goal classes. The types of goals extend the Goal class (inheritance). Added constructor and getters to goal class and the createGoal(), addGoal(), removeGoal() and checkGoals() methods to the Goals class.
    
    Began implementation of Goal classes. The types of goals extend the Goal class (inheritance). Added constructor and getters to goal class and the createGoal(), addGoal(), removeGoal() and checkGoals() methods to the Goals class.

---
+++ src/main/java/seng202/team10/Model/FrequencyGoal.java
@@ -0,0 +1,9 @@
+package seng202.team10.Model;
+
+public class FrequencyGoal extends Goal {
+
+    public FrequencyGoal() {
+        super(10, 13, "Frequency", false, 0.0, 5, 0.0, 0.0, 0.0);
+    }
+
+}
