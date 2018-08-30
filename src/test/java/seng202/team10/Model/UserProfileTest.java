142a34b88f4f7cea60294eba02adccf91850355a
Cam Arnold <cta67@uclive.ac.nz>
2018-08-30 14:22:53 +1200

    Making classes implement serializable

--- src/test/java/seng202/team10/Model/UserProfileTest.java
+++ src/test/java/seng202/team10/Model/UserProfileTest.java
@@ -1,15 +1,32 @@
 package seng202.team10.Model;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.FileOperations.FileReader;
+import seng202.team10.Model.FileOperations.FileWriter;
+import seng202.team10.Model.FileOperations.Parser;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class UserProfileTest {
 
+    private UserProfile testProfile = new UserProfile();
+    private Parser testParser = new Parser();
+    private FileReader reader= new FileReader();
+    private FileWriter writer = new FileWriter();
+
+
     @Before
     public void setUp() throws Exception {
+        testProfile.setName("dave");
+        testProfile.setWeight(80);
+        ArrayList<String> fileContents = testParser.getFileContents("testdata.csv");
+        ArrayList<ArrayList<String>> formattedFile = testParser.formatFileContents(fileContents);
+        ArrayList<Activity> testResults = testParser.processFile(formattedFile);
+        testProfile.addActivities(testResults);
     }
 
     @After
@@ -17,6 +34,15 @@ public class UserProfileTest {
     }
 
     @Test
-    public void addActivity() {
+    public void serializeTest(){
+        writer.saveProfile(testProfile);
+        UserProfile loadedProfile = reader.loadExistingProfile("dave");
+        assertEquals(testProfile.getActivities(), loadedProfile.getActivities());
+    }
+
+    @Test
+    public void addActivities() {
+
+
     }
 }
