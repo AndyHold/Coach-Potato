9b2baa97545643188c20842b7b28e2329b887c92
Torben <tkl34@uclive.ac.nz>
2018-08-31 11:48:28 +1200

    GUIController now working with two screens, CreateProfile and Login. There is partial functionality, as the login button does nothing currently and the create profile fails on invalid data. Clicking create profile creates a new UserProfile with the data entered.

--- src/main/java/seng202/team10/Control/GUIController.java
+++ src/main/java/seng202/team10/Control/GUIController.java
@@ -1,5 +1,60 @@
 package seng202.team10.Control;
 
-public class GUIController {
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import seng202.team10.GUI.CreateProfileControl;
+import seng202.team10.GUI.LoginControl;
+
+public class GUIController extends Application {
+
+    private FXMLLoader loginLoader;
+    private Scene loginScene;
+    private FXMLLoader createProfileLoader;
+    private Scene createProfileScene;
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        loadAllScenes();
+        primaryStage.setTitle("Coach Potato");
+        primaryStage.setScene(loginScene);
+        primaryStage.show();
+        this.primaryStage = primaryStage;
+    }
+
+    public void launchLoginScene() throws Exception {
+        primaryStage.setScene(loginScene);
+    }
+
+//    public void launchProfileScene() {
+//
+//    }
+
+    public void launchCreateProfileScene() throws Exception {
+        createProfileLoader.getController();
+        primaryStage.setScene(createProfileScene);
+    }
+
+    public void loadAllScenes() throws Exception{
+        loginLoader = new FXMLLoader(getClass().getResource("/fxml/loginScreen.fxml"));
+        Parent root = loginLoader.load();
+        LoginControl loginControl = loginLoader.getController();
+        loginControl.setApp(this);
+        loginScene = new Scene(root, 800, 400);
+
+        createProfileLoader = new FXMLLoader(getClass().getResource("/fxml/createProfileScreen.fxml"));
+        GridPane gridPane = createProfileLoader.load();
+        CreateProfileControl createProfileControl = createProfileLoader.getController();
+        createProfileControl.setApp(this);
+        createProfileControl.setUpScene();
+        createProfileScene = new Scene(gridPane, 800, 400);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
