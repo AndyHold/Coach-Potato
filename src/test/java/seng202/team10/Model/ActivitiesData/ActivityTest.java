01c8a6c9b33a55dc0fe9eb1143b7e626e5353840
Andy Holden <ash102@uclive.ac.nz>
2018-08-30 14:58:40 +1200

    Updated Activities Class with new constructor and modified Parser to match this new format. Also created ActivityTest file.

---
+++ src/test/java/seng202/team10/Model/ActivitiesData/ActivityTest.java
@@ -0,0 +1,31 @@
+package seng202.team10.Model.ActivitiesData;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActivityTest {
+
+
+
+
+    @Test
+    public void addEntry() {
+    }
+
+    @Test
+    public void calculateAverageVelocity() {
+    }
+
+    @Test
+    public void calculateAverageHeartRate() {
+    }
+
+    @Test
+    public void calculateTotalDistance() {
+    }
+
+    @Test
+    public void calculateTotalDuration() {
+    }
+}
