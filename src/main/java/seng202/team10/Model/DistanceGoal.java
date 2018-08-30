b77ff2d3deb8501aa1b4debc3d30ea6cb35dc522
Paddy <pmm118@uclive.ac.nz>
2018-08-30 15:20:45 +1200

    Got rid of Velocity goal (not appropriate for target users). Wrote review progress functions for each goal type. Added constrctor to Goals class so that the userProfile object is passed through. Changed checkGoal func to use a switch statement instead on many if-else's.
    Got rid of Velocity goal (not appropriate for target users). Wrote review progress functions for each goal type. Added constrctor to Goals class so that the userProfile object is passed through. Changed checkGoal func to use a switch statement instead on many if-else's.
    
    Got rid of Velocity goal (not appropriate for target users). Wrote review progress functions for each goal type. Added constrctor to Goals class so that the userProfile object is passed through. Changed checkGoal func to use a switch statement instead on many if-else's.
    
    Got rid of Velocity goal (not appropriate for target users). Wrote review progress functions for each goal type. Added constrctor to Goals class so that the userProfile object is passed through. Changed checkGoal func to use a switch statement instead on many if-else's.

--- src/main/java/seng202/team10/Model/DistanceGoal.java
+++ src/main/java/seng202/team10/Model/DistanceGoal.java
@@ -7,7 +7,7 @@ public class DistanceGoal extends Goal {
     public DistanceGoal(String name, DateTime start, DateTime target, double distance) {
 
 
-        super(name, start, target, "Distance", false, 0.0, 0, 0.0, 0.0, distance, 0.0);
+        super(name, start, target, "Distance", false, 0, 0.0, 0.0, distance, 0.0);
     }
 
 }
