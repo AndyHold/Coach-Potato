374e28aef3a0978bb52c2637e26d5a968ab357b2
Torben <tkl34@uclive.ac.nz>
2018-08-31 15:23:20 +1200

    Added commented out lines for use with other GUI scenes in GUIController. MainController commented out, using GUIController as singular controller now. Users added now show up in the login scene when selecting a user. Error label done for login scene. No additional error checking done for CreateProfileScene.

--- src/main/java/seng202/team10/Control/GUIController.java
+++ src/main/java/seng202/team10/Control/GUIController.java
@@ -8,14 +8,40 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import seng202.team10.GUI.CreateProfileControl;
 import seng202.team10.GUI.LoginControl;
+import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.FileOperations.FileWriter;
+import seng202.team10.Model.FileOperations.Parser;
+import seng202.team10.Model.UserProfile;
+
+import java.util.ArrayList;
 
 public class GUIController extends Application {
 
     private FXMLLoader loginLoader;
     private Scene loginScene;
+    private LoginControl loginController;
+
     private FXMLLoader createProfileLoader;
     private Scene createProfileScene;
+    private CreateProfileControl createProfileController;
+
+//    private FXMLLoader profileLoader;
+//    private Scene profileScene;
+//    private ProfileController profileController;
+
+//    private FXMLLoader goalsLoader;
+//    private Scene goalsScene;
+//    private GoalsController goalsController;
+
+//    private FXMLLoader uploadDataLoader;
+//    private Scene uploadDataScene;
+//    private UploadDataController uploadDataController;
+
     private Stage primaryStage;
+    private ArrayList<UserProfile> users = new ArrayList<UserProfile>();
+    private UserProfile currentUser;
+    private Parser parser = new Parser();
+    private FileWriter dataWriter = new FileWriter();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -26,32 +52,98 @@ public class GUIController extends Application {
         this.primaryStage = primaryStage;
     }
 
+//    public void setMainController(MainController mainController) {
+//        this.mainController = mainController;
+//    }
+
+    public void uploadDataToUser(UserProfile user, String filePath) throws Exception{
+        ArrayList<String> fileContents = parser.getFileContents(filePath);
+        ArrayList<ArrayList<String>> formattedFileContents = parser.formatFileContents(fileContents);
+        ArrayList<Activity> activities = parser.processFile(formattedFileContents);
+        user.addActivities(activities);
+    }
+
     public void launchLoginScene() throws Exception {
+        loginController.setUpScene();
         primaryStage.setScene(loginScene);
     }
 
 //    public void launchProfileScene() {
-//
+//      profileController.setUpScene();
+//      primaryStage.setScene(profileScene);
+//    }
+
+//    public void launchGoalsScene() {
+//      goalsController.setUpScene();
+//      primaryStage.setScene(goalsScene);
+//    }
+
+//    public void launchUploadDataScene() {
+//      uploadDataController.setUpScene();
+//      primaryStage.setScene(uploadDataScene);
 //    }
 
     public void launchCreateProfileScene() throws Exception {
-        createProfileLoader.getController();
         primaryStage.setScene(createProfileScene);
     }
 
     public void loadAllScenes() throws Exception{
         loginLoader = new FXMLLoader(getClass().getResource("/fxml/loginScreen.fxml"));
         Parent root = loginLoader.load();
-        LoginControl loginControl = loginLoader.getController();
-        loginControl.setApp(this);
+        loginController = loginLoader.getController();
+        loginController.setApp(this);
+        loginController.setUpScene();
         loginScene = new Scene(root, 800, 400);
 
         createProfileLoader = new FXMLLoader(getClass().getResource("/fxml/createProfileScreen.fxml"));
-        GridPane gridPane = createProfileLoader.load();
-        CreateProfileControl createProfileControl = createProfileLoader.getController();
-        createProfileControl.setApp(this);
-        createProfileControl.setUpScene();
-        createProfileScene = new Scene(gridPane, 800, 400);
+        GridPane gridPaneCP = createProfileLoader.load();
+        createProfileController = createProfileLoader.getController();
+        createProfileController.setApp(this);
+        createProfileController.setUpScene();
+        createProfileScene = new Scene(gridPaneCP, 800, 400);
+
+//        profileLoader = new FXMLLoader(getClass().getResource("/fxml/profileScreen.fxml"));
+//        GridPane gridPaneP = profileLoader.load();
+//        ProfileController profileController = profileLoader.getController();
+//        profileController.setApp(this);
+//        profileController.setUpScene();
+//        profileScene = new Scene(gridPaneP, 900, 600);
+
+//        goalsLoader = new FXMLLoader(getClass().getResource("/fxml/goalsScreen.fxml"));
+//        GridPane gridPaneG = goalsLoader.load();
+//        goalsController = goalsLoader.getController();
+//        goalsController.setApp(this);
+//        goalsController.setUpScene();
+//        goalsScene = new Scene(gridPaneG, 900, 600);
+
+//        uploadDataLoader = new FXMLLoader(getClass().getResource("/fxml/uploadDataScreen.fxml"));
+//        Pane paneUD = uploadDataLoader.load();
+//        uploadDataController = uploadDataLoader.getController();
+//        uploadDataController.setApp(this);
+//        uploadDataController.setUpScene();
+//        uploadDataScene = new Scene(paneUD, 900, 600);
+    }
+
+    public void createUser(UserProfile newUser) throws Exception{
+        users.add(newUser);
+        setCurrentProfile(newUser);
+        uploadDataToUser(currentUser, "testdata.csv");
+    }
+
+    public void setCurrentProfile(UserProfile userProfile) {
+        this.currentUser = userProfile;
+    }
+
+    public UserProfile getCurrentProfile() {
+        return this.currentUser;
+    }
+
+    public ArrayList<UserProfile> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<UserProfile> users) {
+        this.users = users;
     }
 
     public static void main(String[] args) {
--- src/main/java/seng202/team10/GUI/LoginControl.java
+++ src/main/java/seng202/team10/GUI/LoginControl.java
@@ -1,32 +1,66 @@
 package seng202.team10.GUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import seng202.team10.Control.GUIController;
+import seng202.team10.Model.UserProfile;
 
 public class LoginControl {
     private GUIController app;
 
     @FXML
-    Button loginButton;
+    private Button loginButton;
 
     @FXML
-    Button newProfileButton;
+    private Button newProfileButton;
+
+    @FXML
+    private ComboBox<String> userComboBox;
+
+    @FXML
+    private Label userErrorLabel;
 
 
     public void setApp(GUIController app) {
         this.app = app;
     }
 
+    public void setUpScene() {
+        ObservableList<String> userNames = FXCollections.observableArrayList();
+        for (UserProfile user : app.getUsers()) {
+            userNames.add(user.getName());
+        }
+        System.out.println(userNames);
+        userComboBox.setItems(userNames);
+        userComboBox.setVisibleRowCount(10);
+        userErrorLabel.setVisible(false);
+    }
+
     @FXML
     public void login() {
+        setUpScene();
         loginButton.setText("A thing happened.");
+        String userName = userComboBox.getValue();
+        if (userName == null) {
+            userErrorLabel.setVisible(true);
+        } else {
+//            for (UserProfile user : users) {
+//                if (userName == user.getName()) {
+////                    app.launchCreateProfileScene(user);
+//                }
+//            }
+        }
 //        app.launchProfileScene(user);
 
     }
 
     @FXML
     public void createProfile() throws Exception {
+        setUpScene();
         app.launchCreateProfileScene();
     }
 
