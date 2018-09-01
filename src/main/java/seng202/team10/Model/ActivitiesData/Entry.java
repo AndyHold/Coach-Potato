bcf8aa7fbe0f5e24abe0f815ea1f3a50b5b0dfa4
Andy Holden <ash102@uclive.ac.nz>
2018-09-01 18:26:17 +1200

    Modified table in Data Upload Screen so that it can edit values and input entries. Code is messy but works so will require a refactor

--- src/main/java/seng202/team10/Model/ActivitiesData/Entry.java
+++ src/main/java/seng202/team10/Model/ActivitiesData/Entry.java
@@ -1,6 +1,8 @@
 package seng202.team10.Model.ActivitiesData;
 
 
+import javafx.beans.property.SimpleStringProperty;
+
 import java.io.Serializable;
 
 /**
@@ -12,8 +14,14 @@ public class Entry  implements Serializable {
 
     private boolean firstEntry;
     private DateTime time;
+    private SimpleStringProperty timeString;
+    private SimpleStringProperty dateString;
+    private SimpleStringProperty heartRateString;
     private double heartRate;
     private Position position;
+    private SimpleStringProperty longitudeString;
+    private SimpleStringProperty latitudeString;
+    private SimpleStringProperty elevationString;
     private double distance = 0;
     private double velocity = 0;
     //private Activity activity;
@@ -50,8 +58,149 @@ public class Entry  implements Serializable {
         this.time = time;
         this.heartRate = heartRate;
         this.position = position;
+        setStrings();
         //this.activity = activity;
     }
+
+
+    /**
+     * Setter method for the string representations for the GUI Table View
+     */
+    private void setStrings()
+    {
+        this.dateString = this.time.getDate();
+        this.timeString = this.time.getTime();
+        this.heartRateString = new SimpleStringProperty(String.valueOf(this.heartRate));
+        this.latitudeString = new SimpleStringProperty(String.format("%.8f", this.position.getLatitude()));
+        this.longitudeString = new SimpleStringProperty(String.format("%.8f", this.position.getLongitude()));
+        this.elevationString = new SimpleStringProperty(String.valueOf(this.position.getElevation()));
+    }
+
+
+    /**
+     * Method to change the current date of the entry (for GUI purposes)
+     * @param newDate String: String representation of a Date
+     * @throws IllegalArgumentException if illegal arguments encountered
+     */
+    public void changeDate(String newDate) throws IllegalArgumentException
+    {
+        String[] dateArray = newDate.split("/");
+        int day = Integer.valueOf(dateArray[0]);
+        int month = Integer.valueOf(dateArray[1]);
+        int year = Integer.valueOf(dateArray[2]);
+        this.time.changeDate(year, month, day);
+    }
+
+
+    /**
+     * Method to change the current date of the entry (for GUI purposes)
+     * @param newDate String: String representation of a Time
+     * @throws IllegalArgumentException if illegal arguments encountered
+     */
+    public void changeTime(String newDate) throws IllegalArgumentException
+    {
+        String[] timeArray = newDate.split(":");
+        int hour = Integer.valueOf(timeArray[0]);
+        int minute = Integer.valueOf(timeArray[1]);
+        int second = Integer.valueOf(timeArray[2]);
+        this.time.changeTime(hour, minute, second);
+    }
+
+
+    /**
+     * Method to change the current heart rate of the entry (for GUI purposes)
+     * @param newHeartRate String: String representation of the heart rate double
+     * @throws NumberFormatException String does not represent a number
+     */
+    public void changeHeartRate(String newHeartRate) throws NumberFormatException
+    {
+        this.heartRate = Double.valueOf(newHeartRate);
+    }
+
+
+    /**
+     * Method to change the current latitude of the entry (for GUI purposes)
+     * @param newLatitude String: String representation of the heart rate double
+     * @throws NumberFormatException String does not represent a number
+     */
+    public void changeLatitude(String newLatitude) throws NumberFormatException
+    {
+        this.position.setLatitude(Double.valueOf(newLatitude));
+    }
+
+
+    /**
+     * Method to change the current longitude of the entry (for GUI purposes)
+     * @param newLongitude String: String representation of the heart rate double
+     * @throws NumberFormatException String does not represent a number
+     */
+    public void changeLongitude(String newLongitude) throws NumberFormatException
+    {
+        this.position.setLongitude(Double.valueOf(newLongitude));
+    }
+
+
+    /**
+     * Method to change the current elevation of the entry (for GUI purposes)
+     * @param newElevation String: String representation of the heart rate double
+     * @throws NumberFormatException String does not represent a number
+     */
+    public void changeElevation(String newElevation) throws NumberFormatException
+    {
+        this.position.setElevation(Double.valueOf(newElevation));
+    }
+
+
+    /**
+     * Getter method for the String representation of the date
+     */
+    public String getDateString()
+    {
+        return this.dateString.get();
+    }
+
+
+    /**
+     * Getter method for the String representation of the time
+     */
+    public String getTimeString()
+    {
+        return this.timeString.get();
+    }
+
+
+    public String getHeartRateString()
+    {
+        return this.heartRateString.get();
+    }
+
+
+    /**
+     * Getter method for the String representation of the latitude
+     */
+    public String getLatitudeString()
+    {
+        return this.latitudeString.get();
+    }
+
+
+    /**
+     * Getter method for the String representation of the longitude
+     */
+    public String getLongitudeString()
+    {
+        return this.longitudeString.get();
+    }
+
+
+    /**
+     * Getter method for the String representation of the elevation
+     */
+    public String getElevationString()
+    {
+        return this.elevationString.get();
+    }
+
 
     /**
      * Getter method to check if it is user's first entry.
