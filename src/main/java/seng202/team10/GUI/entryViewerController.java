5566cdc77e296d3a4ff6ceb7748a20da1cf819a5
Cam Arnold <cta67@uclive.ac.nz>
2018-09-02 15:55:25 +1200

    creations of a couple controllers and changes to fxml
    creations of a couple controllers and changes to fxml
    
    creations of a couple controllers and changes to fxml
    
    creations of a couple controllers and changes to fxml

---
+++ src/main/java/seng202/team10/GUI/entryViewerController.java
@@ -0,0 +1,17 @@
+package seng202.team10.GUI;
+
+import seng202.team10.Control.GUIController;
+
+public class entryViewerController {
+
+    private GUIController app;
+
+    /**
+     * Setter method to set the GUI controller for this Scene
+     * @param app GUIController
+     */
+    public void setApp(GUIController app)
+    {
+        this.app = app;
+    }
+}
