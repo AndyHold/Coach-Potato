3825532e7bf77f5d7f57938f1fe52b2aba3fb2c6
Priyesh <psh76@uclive.ac.nz>
2018-09-02 16:52:47 +1200

    Implemented Profile Screen and the Calendar and merged bth screens. Added getBMI method to UserProfile.

--- src/main/java/seng202/team10/Model/UserProfile.java
+++ src/main/java/seng202/team10/Model/UserProfile.java
@@ -129,6 +129,10 @@ public class UserProfile implements java.io.Serializable {
         return this.bmi;
     }
 
+    public double getBmi() {
+        return bmi;
+    }
+
 
     @Override
     public String toString() {
