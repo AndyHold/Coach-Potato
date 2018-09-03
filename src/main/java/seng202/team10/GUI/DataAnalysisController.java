56b9484fc627f129ecbfd0dfa9e055144cd41710
Torben <tkl34@uclive.ac.nz>
2018-09-03 15:01:43 +1200

    Added some implementation for a data analysis controller. Added a new fxml scene for data analysis. Addeed a stub for testing data analysis.
    Added some implementation for a data analysis controller.
    Added a new fxml scene for data analysis.
    Addeed a stub for testing data analysis.
    
    Added some implementation for a data analysis controller.
    Added a new fxml scene for data analysis.
    Addeed a stub for testing data analysis.
    
    Added some implementation for a data analysis controller.
    Added a new fxml scene for data analysis.
    Addeed a stub for testing data analysis.

---
+++ src/main/java/seng202/team10/GUI/DataAnalysisController.java
@@ -0,0 +1,86 @@
+package seng202.team10.GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import seng202.team10.Control.DataAnalysis;
+import seng202.team10.Control.GUIController;
+import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.ActivitiesData.DateTime;
+
+import java.util.ArrayList;
+
+public class DataAnalysisController implements Controllable{
+
+    private GUIController guiController;
+    private Activity activity;
+    private DataAnalysis dataAnalysis;
+
+    @FXML
+    private Label activityNameLabel;
+
+    @FXML
+    private Label timeTakenLabel;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private LineChart distanceOverTime;
+
+    @FXML
+    private LineChart heartRateOverTime;
+
+    @FXML
+    private LineChart caloriesBurned;
+
+    @FXML
+    private LineChart stressLevelOverTime;
+
+    @Override
+    public void setApp(GUIController guiController) {
+        this.guiController = guiController;
+    }
+
+    @Override
+    public void setUpScene() {
+        dataAnalysis = new DataAnalysis();
+        activityNameLabel.setText(activity.getName());
+        ArrayList<DateTime> timeArray = dataAnalysis.getTimeFromActivity(activity);
+//        DateTime startTime = activity.getStartDateTime();
+//        DateTime endTime = activity.getTotalDuration();
+//        startTime.subtract(endTime);
+//        timeTakenLabel.setText();
+
+        XYChart.Series dtSeries = new XYChart.Series();
+        ArrayList<Double> distanceArray = dataAnalysis.getDistanceFromActivity(activity);
+        for (int i = 0; i < timeArray.size(); i++) {
+            dtSeries.getData().add(new XYChart.Data(timeArray.get(i), distanceArray.get(i)));
+        }
+        distanceOverTime.getData().add(dtSeries);
+
+        XYChart.Series hrtSeries = new XYChart.Series();
+        ArrayList<Double> heartRateArray = dataAnalysis.getHeartRateFromActivity(activity);
+        for (int i = 0; i < timeArray.size(); i++) {
+            hrtSeries.getData().add(new XYChart.Data(timeArray.get(i), heartRateArray.get(i)));
+        }
+        heartRateOverTime.getData().add(hrtSeries);
+
+//        XYChart.Series cbSeries = new XYChart.Series();
+//        ArrayList<Double> calorieArray = dataAnalysis.getCaloriesFromActivity(activity);
+//        for (int i = 0; i < timeArray.size(); i++) {
+//            cbSeries.getData().add(new XYChart.Data(timeArray.get(i), calorieArray.get(i)));
+//        }
+//        caloriesBurned.getData().add(cbSeries);
+
+//        XYChart.Series sltSeries = new XYChart.Series();
+//        ArrayList<Double> stressArray = dataAnalysis.getStressFromActivity(activity);
+//        for (int i = 0; i < timeArray.size(); i++) {
+//            sltSeries.getData().add(new XYChart.Data(timeArray.get(i), stressArray.get(i)));
+//        }
+//        stressLevelOverTime.getData().add(sltSeries);
+
+    }
+}
