fa1902a7b03b3fd13e5e574679216921f0c18f60
Torben <tkl34@uclive.ac.nz>
2018-09-05 17:38:35 +1200

    Uncommented the functionality, and added a case for there being no data. Now displays a distance/time graph.
    Uncommented the functionality, and added a case for there being no data.
    Now displays a distance/time graph.
    
    Uncommented the functionality, and added a case for there being no data.
    Now displays a distance/time graph.
    
    Uncommented the functionality, and added a case for there being no data.
    Now displays a distance/time graph.

--- src/main/java/seng202/team10/GUI/DataAnalysisController.java
+++ src/main/java/seng202/team10/GUI/DataAnalysisController.java
@@ -2,11 +2,15 @@ package seng202.team10.GUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import seng202.team10.Control.DataAnalysis;
 import seng202.team10.Control.GUIController;
 import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.ActivitiesData.DateTime;
+
+import java.util.ArrayList;
 
 public class DataAnalysisController implements Controllable{
 
@@ -14,26 +18,14 @@ public class DataAnalysisController implements Controllable{
     private Activity activity;
     private DataAnalysis dataAnalysis;
 
-    @FXML
-    private Label activityNameLabel;
-
-    @FXML
-    private Label timeTakenLabel;
-
-    @FXML
-    private TabPane tabPane;
-
-    @FXML
-    private LineChart distanceOverTime;
-
-    @FXML
-    private LineChart heartRateOverTime;
-
-    @FXML
-    private LineChart caloriesBurned;
-
-    @FXML
-    private LineChart stressLevelOverTime;
+    @FXML private Label activityNameLabel;
+    @FXML private Label timeTakenLabel;
+    @FXML private TabPane tabPane;
+    @FXML private LineChart distanceOverTime;
+    @FXML private LineChart heartRateOverTime;
+    @FXML private LineChart caloriesBurned;
+    @FXML private LineChart stressLevelOverTime;
+    @FXML private Label noActivitiesLabel;
 
     @Override
     public void setApp(GUIController guiController) {
@@ -42,47 +34,66 @@ public class DataAnalysisController implements Controllable{
 
     @Override
     public void setUpScene() {
-//        dataAnalysis = new DataAnalysis();
-//        activityNameLabel.setText(activity.getName());
-//        ArrayList<Integer> timeArray = dataAnalysis.getTimeFromActivity(activity);
-//        DateTime startTime = activity.getStartDateTime();
-//        DateTime endTime = activity.getTotalDuration();
-//        startTime.subtract(endTime);
-//        timeTakenLabel.setText();
+        if (guiController.getCurrentProfile().getActivities().isEmpty()) {
+            this.displayNoData(true);
+        } else {
+            this.displayNoData(false);
+            activity = guiController.getCurrentProfile().getActivities().get(0);
+            dataAnalysis = new DataAnalysis();
+            activityNameLabel.setText(activity.getName());
+            ArrayList<Integer> timeArray = dataAnalysis.getTimeFromActivity(activity);
+            DateTime startTime = activity.getStartDateTime();
+            Integer timeTaken = activity.getTotalDuration();
+            timeTakenLabel.setText(timeTaken.toString());
 
-//        XYChart.Series dtSeries = new XYChart.Series();
-//        ArrayList<Double> distanceArray = dataAnalysis.getDistanceFromActivity(activity);
-//        double totalDistance = 0;
-//        for (int i = 0; i < timeArray.size(); i++) {
-//            totalDistance = totalDistance + distanceArray.get(i);
-//            dtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), totalDistance));
-//        }
-//        System.out.println(totalDistance);
-//        System.out.println(distanceArray);
-//        distanceOverTime.getData().add(dtSeries);
-//
-//        XYChart.Series hrtSeries = new XYChart.Series();
-//        ArrayList<Integer> heartRateArray = dataAnalysis.getHeartRateFromActivity(activity);
-//        for (int i = 0; i < timeArray.size(); i++) {
-//            hrtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), heartRateArray.get(i)));
-//        }
-//        heartRateOverTime.getData().add(hrtSeries);
+            XYChart.Series dtSeries = new XYChart.Series();
+            ArrayList<Double> distanceArray = dataAnalysis.getDistanceFromActivity(activity);
+            double totalDistance = 0;
+            for (int i = 0; i < timeArray.size(); i++) {
+                totalDistance = totalDistance + distanceArray.get(i);
+                dtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), totalDistance));
+            }
+            System.out.println(totalDistance);
+            System.out.println(distanceArray);
+            distanceOverTime.getData().add(dtSeries);
 
-//        XYChart.Series cbSeries = new XYChart.Series();
-//        ArrayList<Double> calorieArray = dataAnalysis.getCaloriesFromActivity(activity);
-//        for (int i = 0; i < timeArray.size(); i++) {
-//            cbSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), calorieArray.get(i)));
-//        }
-//        caloriesBurned.getData().add(cbSeries);
+            XYChart.Series hrtSeries = new XYChart.Series();
+            ArrayList<Integer> heartRateArray = dataAnalysis.getHeartRateFromActivity(activity);
+            for (int i = 0; i < timeArray.size(); i++) {
+                hrtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), heartRateArray.get(i)));
+            }
+            heartRateOverTime.getData().add(hrtSeries);
 
-//        XYChart.Series sltSeries = new XYChart.Series();
-//        ArrayList<Double> stressArray = dataAnalysis.getStressFromActivity(activity);
-//        for (int i = 0; i < timeArray.size(); i++) {
-//            sltSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), stressArray.get(i)));
-//        }
-//        stressLevelOverTime.getData().add(sltSeries);
+    //        XYChart.Series cbSeries = new XYChart.Series();
+    //        ArrayList<Double> calorieArray = dataAnalysis.getCaloriesFromActivity(activity);
+    //        for (int i = 0; i < timeArray.size(); i++) {
+    //            cbSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), calorieArray.get(i)));
+    //        }
+    //        caloriesBurned.getData().add(cbSeries);
+    //
+    //        XYChart.Series sltSeries = new XYChart.Series();
+    //        ArrayList<Double> stressArray = dataAnalysis.getStressFromActivity(activity);
+    //        for (int i = 0; i < timeArray.size(); i++) {
+    //            sltSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), stressArray.get(i)));
+
+    //        stressLevelOverTime.getData().add(sltSeries);
+        }
 
     }
+    private void displayNoData(boolean noDataFound) {
+        if (noDataFound) {
+            tabPane.setVisible(false);
+            activityNameLabel.setVisible(false);
+            timeTakenLabel.setVisible(false);
+            noActivitiesLabel.setVisible(true);
+        } else {
+            tabPane.setVisible(true);
+            activityNameLabel.setVisible(true);
+            timeTakenLabel.setVisible(true);
+            noActivitiesLabel.setVisible(false);
+        }
+    }
+
 
     public void setActivity(Activity activity) {
         this.activity = activity;
