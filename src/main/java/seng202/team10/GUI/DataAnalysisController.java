13d6661aca52522410c484958934254bbea25a88
Torben <tkl34@uclive.ac.nz>
2018-09-05 12:50:44 +1200

    Commented out many lines to get the dataAnalysis scene working.
    Commented out many lines to get the dataAnalysis scene working.
    
    Commented out many lines to get the dataAnalysis scene working.
    
    Commented out many lines to get the dataAnalysis scene working.

--- src/main/java/seng202/team10/GUI/DataAnalysisController.java
+++ src/main/java/seng202/team10/GUI/DataAnalysisController.java
@@ -2,14 +2,11 @@ package seng202.team10.GUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import seng202.team10.Control.DataAnalysis;
 import seng202.team10.Control.GUIController;
 import seng202.team10.Model.ActivitiesData.Activity;
-
-import java.util.ArrayList;
 
 public class DataAnalysisController implements Controllable{
 
@@ -45,31 +42,31 @@ public class DataAnalysisController implements Controllable{
 
     @Override
     public void setUpScene() {
-        dataAnalysis = new DataAnalysis();
-        activityNameLabel.setText(activity.getName());
-        ArrayList<Integer> timeArray = dataAnalysis.getTimeFromActivity(activity);
+//        dataAnalysis = new DataAnalysis();
+//        activityNameLabel.setText(activity.getName());
+//        ArrayList<Integer> timeArray = dataAnalysis.getTimeFromActivity(activity);
 //        DateTime startTime = activity.getStartDateTime();
 //        DateTime endTime = activity.getTotalDuration();
 //        startTime.subtract(endTime);
 //        timeTakenLabel.setText();
 
-        XYChart.Series dtSeries = new XYChart.Series();
-        ArrayList<Double> distanceArray = dataAnalysis.getDistanceFromActivity(activity);
-        double totalDistance = 0;
-        for (int i = 0; i < timeArray.size(); i++) {
-            totalDistance = totalDistance + distanceArray.get(i);
-            dtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), totalDistance));
-        }
-        System.out.println(totalDistance);
-        System.out.println(distanceArray);
-        distanceOverTime.getData().add(dtSeries);
-
-        XYChart.Series hrtSeries = new XYChart.Series();
-        ArrayList<Integer> heartRateArray = dataAnalysis.getHeartRateFromActivity(activity);
-        for (int i = 0; i < timeArray.size(); i++) {
-            hrtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), heartRateArray.get(i)));
-        }
-        heartRateOverTime.getData().add(hrtSeries);
+//        XYChart.Series dtSeries = new XYChart.Series();
+//        ArrayList<Double> distanceArray = dataAnalysis.getDistanceFromActivity(activity);
+//        double totalDistance = 0;
+//        for (int i = 0; i < timeArray.size(); i++) {
+//            totalDistance = totalDistance + distanceArray.get(i);
+//            dtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), totalDistance));
+//        }
+//        System.out.println(totalDistance);
+//        System.out.println(distanceArray);
+//        distanceOverTime.getData().add(dtSeries);
+//
+//        XYChart.Series hrtSeries = new XYChart.Series();
+//        ArrayList<Integer> heartRateArray = dataAnalysis.getHeartRateFromActivity(activity);
+//        for (int i = 0; i < timeArray.size(); i++) {
+//            hrtSeries.getData().add(new XYChart.Data(timeArray.get(i).toString(), heartRateArray.get(i)));
+//        }
+//        heartRateOverTime.getData().add(hrtSeries);
 
 //        XYChart.Series cbSeries = new XYChart.Series();
 //        ArrayList<Double> calorieArray = dataAnalysis.getCaloriesFromActivity(activity);
