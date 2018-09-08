11f45479a2f53b6adbbc40dd6fc5b3428db12e13
Torben <tkl34@uclive.ac.nz>
2018-09-08 17:04:15 +1200

    Messed around with this to try make it work

--- src/main/java/seng202/team10/GUI/MapController.java
+++ src/main/java/seng202/team10/GUI/MapController.java
@@ -18,11 +18,20 @@ public class MapController implements Controllable{
         this.guiController = guiController;
     }
 
+
+
     @Override
     public void setUpScene() {
-        webEngine = mapWebView.getEngine();
-        webEngine.load("https://www.google.com/maps/@-43.5223396,172.5775872,15z");
 
+
+        webEngine = mapWebView.getEngine();
+        webEngine.load("<iframe src=\"https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11556.353949034337!2d172.61863394979792!3d-43.53000993499655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snz!4v1536374411725\" width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" allowfullscreen></iframe>");
+
+
+    }
+
+    static { // use system proxy settings when standalone application
+        System.setProperty("java.net.useSystemProxies", "true");
     }
 
     @FXML
