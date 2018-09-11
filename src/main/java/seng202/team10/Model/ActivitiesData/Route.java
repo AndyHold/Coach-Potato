5629cf3daa11066db0ee046502303d891f679b43
Torben <tkl34@uclive.ac.nz>
2018-09-11 17:48:37 +1200

    Unimplemented helper class for getting the map to work

---
+++ src/main/java/seng202/team10/Model/ActivitiesData/Route.java
@@ -0,0 +1,28 @@
+package seng202.team10.Model.ActivitiesData;
+
+/**
+ * Created by brad on 9/09/16.
+ */
+public class Route {
+//    private List<Position> route = new ArrayList<>();
+//
+//    public Route(Position ...points) {
+//        Collections.addAll(route, points);
+//    }
+//    public void addPosition(Position position) {
+//        this.route.add(position);
+//    }
+//
+//    public List<Position> getRoute() {
+//        return route;
+//    }
+//
+//    public String toJSONArray() {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append("[");
+//        route.forEach(pos -> stringBuilder.append(
+//                String.format("{lat: %f, lng: %f}, ", pos.getLatitude(), pos.getLongitude())));
+//        stringBuilder.append("]");
+//        return stringBuilder.toString();
+//    }
+}
