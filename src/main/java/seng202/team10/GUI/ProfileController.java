0171c236123df3ec986733a4e689dd406fbe8e4d
Priyesh <psh76@uclive.ac.nz>
2018-09-11 17:26:16 +1200

    1)Linked all the screens together so that we can go to any screen from any screen. 2)deleted unused files. 3)Fixed calendar. 4) Gui improvement on all screens.

--- src/main/java/seng202/team10/GUI/ProfileController.java
+++ src/main/java/seng202/team10/GUI/ProfileController.java
@@ -12,6 +12,10 @@ import seng202.team10.Model.UserProfile;
 
 import java.text.DecimalFormat;
 import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class ProfileController {
 
@@ -19,7 +23,7 @@ public class ProfileController {
 
     @FXML Pane calendarPane;
     @FXML private javafx.scene.control.Label welcomeProfileLabel;
-    @FXML private javafx.scene.control.Label qoutesLabel;
+    @FXML private javafx.scene.control.Label quotesLabel;
     @FXML private javafx.scene.control.Label weightValueLabel;
     @FXML private javafx.scene.control.Label heightValueLabel;
     @FXML private javafx.scene.control.Label bmiValueLabel;
@@ -36,6 +40,27 @@ public class ProfileController {
     @FXML private Text velocityText;
     @FXML private Text heartRateText;
     @FXML private VBox drawer;
+    private List<String> quotes = Arrays.asList(
+            "To not prepare is to prepare to fail.",
+            "Exercise instead of extra fries.",
+            "It starts in the kitchen and continues on the pavement.",
+            "People say nothing is impossible, but I do nothing everyday.",
+            "If you cannot do great things, do small things in a great way.",
+            "You are what you repeatedly do.",
+            "It is our choices, Harry that show what we truly are, far more than our abilities.",
+            "People often say that motivation doesn't last. Well, neither does bathing - that's why we recommend it daily.",
+            "I didn't get there by wishing for it or hoping for it, but by working for it.",
+            "Be patient with yourself. Self-growth is tender; it’s holy ground. There’s no greater investment.",
+            "You can waste your life drawing lines. Or you can live your life crossing them.",
+            "Action is the foundational key to all success.",
+            "Obstacles are those frightful things you see when you take your eyes off your goal.",
+            "Someday is not a day of the week.",
+            "Challenges are what make life interesting and overcoming them is what makes life meaningful.",
+            "I already know what giving up feels like, I want to see what happens if i don't.",
+            "Some days it's hard to find motivation, some days motivation finds you!",
+            "Yesterday you said tomorrow, so just do it!"
+
+    );
 
 
     public void setApp(GUIController app){
@@ -50,6 +75,7 @@ public class ProfileController {
         distanceHBox.setVisible(false);
         velocityHBox.setVisible(false);
         heartRateHBox.setVisible(false);
+        quotesLabel.setText(quotes.get((int)(Math.random()*(quotes.size()))));
     }
 
 
