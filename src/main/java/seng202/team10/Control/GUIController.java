880547c15707728715359c05f3d4b8776f610bf8
Priyesh <psh76@uclive.ac.nz>
2018-09-13 15:49:22 +1200

    Created constructors for userProfile and Added a test user.

--- src/main/java/seng202/team10/Control/GUIController.java
+++ src/main/java/seng202/team10/Control/GUIController.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import seng202.team10.GUI.*;
 import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.ActivitiesData.DateTime;
 import seng202.team10.Model.FileOperations.FileWriter;
 import seng202.team10.Model.FileOperations.Parser;
 import seng202.team10.Model.Goals;
