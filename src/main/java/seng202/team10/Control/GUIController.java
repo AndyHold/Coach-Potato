0fbb14ad24a30a632bf33d4b7994ddc34e8bdace
Torben <tkl34@uclive.ac.nz>
2018-09-13 15:52:28 +1200

    Added functionality for the map controller

--- src/main/java/seng202/team10/Control/GUIController.java
+++ src/main/java/seng202/team10/Control/GUIController.java
@@ -4,12 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import seng202.team10.GUI.*;
 import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.ActivitiesData.Route;
 import seng202.team10.Model.FileOperations.FileWriter;
 import seng202.team10.Model.FileOperations.Parser;
 import seng202.team10.Model.Goals;
@@ -136,6 +135,14 @@ public class GUIController extends Application{
         primaryStage.setScene(profileScene);
 //        profileController.setUserDetails();
 
+    }
+
+    public void launchMapScene(Activity activity) {
+        mapController.setActivity(activity);
+        mapController.setUpScene();
+        Route newRoute = mapController.makeRoute(activity);
+        mapController.displayRoute(newRoute);
+        primaryStage.setScene(mapScene);
     }
 
     public void launchGoalsScene() {
