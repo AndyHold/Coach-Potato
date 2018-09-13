6460ad3ae42846c8ad9de47fc234641689493354
Priyesh <psh76@uclive.ac.nz>
2018-09-13 18:43:58 +1200

    Added image buttons on login Screen, still need to work on it though.

--- src/main/java/seng202/team10/GUI/LoginController.java
+++ src/main/java/seng202/team10/GUI/LoginController.java
@@ -6,23 +6,26 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import seng202.team10.Control.GUIController;
 import seng202.team10.Model.UserProfile;
+
+import java.awt.*;
 
 public class LoginController implements Controllable{
     private GUIController app;
 
-    @FXML
-    private Button loginButton;
+    @FXML private Button loginButton;
+    @FXML private Button newProfileButton;
+    @FXML private ComboBox<String> userComboBox;
+    @FXML private Label userErrorLabel;
+    @FXML private ImageView user1Image;
+    @FXML private ImageView user2Image;
+    @FXML private ImageView user3Image;
+    @FXML private ImageView user4Image;
+    @FXML private ImageView user5Image;
 
-    @FXML
-    private Button newProfileButton;
-
-    @FXML
-    private ComboBox<String> userComboBox;
-
-    @FXML
-    private Label userErrorLabel;
 
     public void setApp(GUIController app) {
         this.app = app;
@@ -36,6 +39,16 @@ public class LoginController implements Controllable{
         for (UserProfile user : app.getUsers()) {
             userNames.add(user.getName());
         }
+        user1Image.setImage(new Image("Images/stick-man.jpg"));
+        user2Image.setImage(new Image("Images/stick-man.jpg"));
+        user3Image.setImage(new Image("Images/stick-man.jpg"));
+        user4Image.setImage(new Image("Images/stick-man.jpg"));
+        user5Image.setImage(new Image("Images/stick-man.jpg"));
+        user1Image.setVisible(true);
+        user2Image.setVisible(true);
+        user3Image.setVisible(true);
+        user4Image.setVisible(true);
+        user5Image.setVisible(true);
         userComboBox.setItems(userNames);
         userComboBox.setVisibleRowCount(10);
         userErrorLabel.setVisible(false);
