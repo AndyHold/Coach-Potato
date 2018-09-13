37372ad85ad3133a1ec7a8ad7b7c397134333ea7
Torben <tkl34@uclive.ac.nz>
2018-09-13 15:54:43 +1200

    Route class updated for using the map

--- src/main/java/seng202/team10/Model/ActivitiesData/Route.java
+++ src/main/java/seng202/team10/Model/ActivitiesData/Route.java
@@ -1,28 +1,32 @@
 package seng202.team10.Model.ActivitiesData;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by brad on 9/09/16.
  */
 public class Route {
-//    private List<Position> route = new ArrayList<>();
-//
-//    public Route(Position ...points) {
-//        Collections.addAll(route, points);
-//    }
-//    public void addPosition(Position position) {
-//        this.route.add(position);
-//    }
-//
-//    public List<Position> getRoute() {
-//        return route;
-//    }
-//
-//    public String toJSONArray() {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        stringBuilder.append("[");
-//        route.forEach(pos -> stringBuilder.append(
-//                String.format("{lat: %f, lng: %f}, ", pos.getLatitude(), pos.getLongitude())));
-//        stringBuilder.append("]");
-//        return stringBuilder.toString();
-//    }
+    private List<Position> route = new ArrayList<>();
+
+    public Route(Position ...points) {
+        Collections.addAll(route, points);
+    }
+    public void addPosition(Position position) {
+        this.route.add(position);
+    }
+
+    public List<Position> getRoute() {
+        return route;
+    }
+
+    public String toJSONArray() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        route.forEach(pos -> stringBuilder.append(
+                String.format("{lat: %f, lng: %f}, ", pos.getLatitude(), pos.getLongitude())));
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
 }
