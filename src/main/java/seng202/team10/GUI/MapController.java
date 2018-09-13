af7ff68ba977f4232143a0afa2c9b3bf735a652d
Torben <tkl34@uclive.ac.nz>
2018-09-13 15:54:20 +1200

    Now mostly functional, still has some issues to work out but displays routes

--- src/main/java/seng202/team10/GUI/MapController.java
+++ src/main/java/seng202/team10/GUI/MapController.java
@@ -1,11 +1,16 @@
 package seng202.team10.GUI;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import seng202.team10.Control.GUIController;
 import seng202.team10.Model.ActivitiesData.Activity;
+import seng202.team10.Model.ActivitiesData.Entry;
+import seng202.team10.Model.ActivitiesData.Route;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,93 +30,82 @@ public class MapController implements Controllable, Initializable{
         this.guiController = guiController;
     }
 
-    @Override
-    public void setUpScene() {
-
-    }
-
-//    private Route routeA = new Route(
-//            new Position(37.772, -122.214, 0),
-//            new Position(21.291,  -157.821, 0),
-//            new Position(-18.142, 178.431, 0),
-//            new Position(-27.467, 153.027, 0)
-//    );
+//    @Override
+//    public void setUpScene() {
 //
-//    private Route routeB = new Route(
-//            new Position(-33.946111, 151.177222, 0),
-//            new Position(-43.489444, 172.532222, 0)
-//    );
+//    }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-//        initMap();
-//        webEngine.getLoadWorker().stateProperty().addListener(
-//                new ChangeListener<Worker.State>() {
-//                    public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
-//                        if (newState == Worker.State.SUCCEEDED) {
-//                            displayRoute(routeA);
-//                        }
-//                    }
-//                });
-//        routeSelection.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
-//            if (newValue == routeARadioButton) {
-//                displayRoute(routeA);
-//            } else {
-//                displayRoute(routeB);
-//            }
-//        });
-    }
-//
+    private Route route;
+
 //    private void initMap() {
 //        webEngine = mapWebView.getEngine();
 //        webEngine.load(MapController.class.getResource("/map.html").toExternalForm());
 //    }
 
-//    private void displayRoute(Route newRoute) {
-//        String scriptToExecute = "displayRoute(" + newRoute.toJSONArray() + ");";
-//        webEngine.executeScript(scriptToExecute);
-//    }
+    public void displayRoute(Route newRoute) {
+        webEngine.getLoadWorker().stateProperty().addListener(
+                new ChangeListener<Worker.State>() {
+                    public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
+                        if (newState == Worker.State.SUCCEEDED) {
+                            String scriptToExecute = "displayRoute(" + newRoute.toJSONArray() + ");";
+                            System.out.println(scriptToExecute);
+                            webEngine.executeScript(scriptToExecute);
+                        }
+                    }
+                });
 
-//    private void displayRoute(Route newRoute) {
-//        String scriptToExecute = "displayRoute(" + newRoute.toJSONArray() + ");";
-//        webEngine.executeScript(scriptToExecute);
-//    }
+
+
+    }
+
+    @Override
+    public void setUpScene() {
+
+        webEngine = mapWebView.getEngine();
+        webEngine.load(this.getClass().getResource("/map.html").toExternalForm());
+
+
 //
-//    @Override
-//    public void setUpScene() {
 //
 //        webEngine = mapWebView.getEngine();
 //        webEngine.load(this.getClass().getResource("/map.html").toExternalForm());
-//
-////        this.activity = guiController.getCurrentProfile().getActivities().get(0);
-////        System.out.println(activity.getEntries());
-//        Route route = new Route(
-//                new Position(30.2553368, -97.83891084, 0)
-//        );
-//
+//        System.out.println("Hello");
+//        route = makeRoute(activity);
+//        System.out.println("Hello there");
 //        this.displayRoute(route);
+//        System.out.println("Howdy");
+    }
 //    }
-//
+
 //    static { // use system proxy settings when standalone application
 //        System.setProperty("java.net.useSystemProxies", "true");
 //    }
-//
-    @FXML
-    public void goToActivityViewer() {
-        System.out.println("boop");
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
     }
-//
-//    @Override
-//    public void initialize(URL location, ResourceBundle resources) {
+
+    @FXML
+    public void goToActivityViewer() throws Exception{
+//        guiController.launchActivityViewerScene();
+        guiController.launchDataAnalysisScene();
+    }
+
+    public Route makeRoute(Activity activity) {
+        Route route = new Route();
+        for (Entry entry : activity.getEntries()) {
+            route.addPosition(entry.getPosition());
+        }
+        return route;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 //        webEngine = mapWebView.getEngine();
 //        webEngine.load(this.getClass().getResource("/map.html").toExternalForm());
-//
-////        this.activity = guiController.getCurrentProfile().getActivities().get(0);
-////        System.out.println(activity.getEntries());
-//        Route route = new Route(
-//                new Position(30.2553368, -97.83891084, 0)
-//        );
-//
-//        this.displayRoute(route);
-//    }
+//        this.activity = guiController.getCurrentProfile().getActivities().get(0);
+//        System.out.println(activity.getEntries());
+
+//        this.displayRoute(routeA);
+    }
 }
