4e355c0cd4aa5ce627e36cbbb01ddaa2959cab2d
Andy Holden <ash102@uclive.ac.nz>
2018-09-14 15:45:54 +1200

    Working on some updates to dataUpload screen, had to change the constructor for the entry Class too.

--- src/main/java/seng202/team10/Model/ExistingActivityException.java
+++ src/main/java/seng202/team10/Model/ExistingActivityException.java
@@ -1,10 +1,15 @@
 package seng202.team10.Model;
 
+
+/**
+ * ExistingActivityException Class for Coach Potato
+ * SENG202 2018S2
+ * @author Andrew Holden, Cam Arnold, Paddy Mitchell, Priyesh Shah, Torben Klausen
+ */
 public class ExistingActivityException extends Exception {
 
     /**
      * Constructor method for ExistingElementException
-     *
      * @param newMessage String: Message to be displayed on exception.
      */
     ExistingActivityException(String newMessage) {
