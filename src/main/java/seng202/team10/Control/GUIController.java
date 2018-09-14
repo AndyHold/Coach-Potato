d7703e8570349e6455f123b9a581f3bbd6bcb6f7
Cam Arnold <cta67@uclive.ac.nz>
2018-09-14 16:07:40 +1200

    loading existing users from file when logging in, deleted some old test users

--- src/main/java/seng202/team10/Control/GUIController.java
+++ src/main/java/seng202/team10/Control/GUIController.java
@@ -27,7 +27,11 @@ import seng202.team10.Model.ActivitiesData.Route;
 =======
 import seng202.team10.Model.ActivitiesData.DateTime;
 import seng202.team10.Model.ActivitiesData.Route;
+<<<<<<< HEAD
 >>>>>>> 874b31ca... Re-added launch mapscene
+=======
+import seng202.team10.Model.FileOperations.FileReader;
+>>>>>>> 88343792... loading existing users from file when logging in, deleted some old test users
 import seng202.team10.Model.FileOperations.FileWriter;
 import seng202.team10.Model.FileOperations.Parser;
 import seng202.team10.Model.Goals;
@@ -89,6 +93,7 @@ public class GUIController extends Application{
     private UserProfile currentUser;
     private Parser parser = new Parser();
     private FileWriter dataWriter = new FileWriter();
+    private FileReader dataReader = new FileReader();
 
     //private Goals goals = new Goals(currentUser);
 
@@ -96,6 +101,7 @@ public class GUIController extends Application{
     public void start(Stage primaryStage) throws Exception{
         // Added a test user.
         users.add(new UserProfile("Potato", 75, 180, new DateTime(2000,1,1,1,1,1), "Male"));
+        loadAllUsers();
         loadAllScenes();
         primaryStage.setTitle("Coach Potato");
 <<<<<<< HEAD
@@ -203,6 +209,17 @@ public class GUIController extends Application{
         primaryStage.show();
         this.primaryStage = primaryStage;
 
+    }
+
+    /**
+     * loads all the users in the profiles folder and adds them to the users arraylist
+     */
+    private void loadAllUsers() {
+        ArrayList<String> userNames = dataReader.getExistingUsers();
+        for(String username: userNames){
+            System.out.println(username);
+            users.add(dataReader.loadExistingProfile(username));
+        }
     }
 
 
