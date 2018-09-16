7b5f5bf9abdd54a9b64037df61960c1fb88109aa
Paddy <pmm118@uclive.ac.nz>
2018-09-16 17:23:37 +1200

    wrote updateGoals function, reviewDistanceGoal function, getGoals, setGoals and getActivitiesDistance functions. Also added the Distance case into the Goals enum. Goals can now access the total distance covered as per the data

--- src/main/java/seng202/team10/Control/GUIController.java
+++ src/main/java/seng202/team10/Control/GUIController.java
@@ -90,7 +90,7 @@ public class GUIController extends Application{
     private Parser parser = new Parser();
     private FileWriter dataWriter = new FileWriter();
 
-    private Goals goals = new Goals(currentUser);
+    //private Goals goals = new Goals(currentUser);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -315,7 +315,9 @@ public class GUIController extends Application{
 >>>>>>> fa48cc98... Re-added launch mapscene
     public void launchGoalsScene() {
         //goalsController.setUpScene();
+        goalsController.updateGoals();
         primaryStage.setScene(goalsScene);
+
     }
 =======
 
@@ -650,9 +652,7 @@ public class GUIController extends Application{
         this.users = users;
     }
 
-    public Goals getGoalsInstance() {
-        return goals;
-    }
+
 
 
     public static void main(String[] args) {
