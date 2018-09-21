5b653f13af744eb1eee900b5c70bf46001d335ad
Priyesh <psh76@uclive.ac.nz>
2018-09-22 06:52:01 +1200

    Created some more tests, this time for Profile Controller. Also added exception messages to the Exceptions.

--- src/main/java/seng202/team10/Model/Exceptions/UniqueNameException.java
+++ src/main/java/seng202/team10/Model/Exceptions/UniqueNameException.java
@@ -1,4 +1,11 @@
 package seng202.team10.Model.Exceptions;
 
 public class UniqueNameException extends Exception {
+    /**
+     * Constructor method for UniqueNameException
+     */
+    public UniqueNameException()
+    {
+        super("This username already exists.");
+    }
 }
