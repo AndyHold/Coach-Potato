c0cec262389a45b0ebf9655b358b73ef0af6ae93
Torben <tkl34@uclive.ac.nz>
2018-09-22 11:13:52 +1200

    Updated some tests and deleted an old file reader

--- src/test/java/seng202/team10/Control/DataAnalysisTest.java
+++ src/test/java/seng202/team10/Control/DataAnalysisTest.java
@@ -32,7 +32,7 @@ public class DataAnalysisTest {
         user.setMaxHeartRate(170);
         DateTime birthday = new DateTime(1996, 12, 04, 0, 0, 0);
         user.setBirthDate(birthday);
-        guiController.uploadDataToUser(user, "/FilesToLoad/testdata.csv");
+        guiController.uploadDataToUser(user, "./FilesToLoad/testdata.csv");
         activities = user.getActivities();
 
     }
@@ -84,7 +84,8 @@ public class DataAnalysisTest {
     }
 
     @Test
-    public void getCaloriesFromActivity() {
+    public void getCaloriesFromActivityMale() {
+        user.setGender("Male");
         ArrayList<Double> calories = dataAnalysis.getCaloriesFromActivity(activities.get(1), user);
         ArrayList<Double> actualcalories = new ArrayList<>();
         int userAge = Calendar.getInstance().get(Calendar.YEAR) - user.getBirthDate().getYear();
@@ -96,6 +97,40 @@ public class DataAnalysisTest {
         actualcalories.add(((-55.0969 + (0.6309 * 147) + (0.1988 * user.getWeight()) + (0.2017 * userAge))/4.184) * 60 * timeArray.get(4)/3600.0);
         actualcalories.add(((-55.0969 + (0.6309 * 143) + (0.1988 * user.getWeight()) + (0.2017 * userAge))/4.184) * 60 * timeArray.get(5)/3600.0);
         actualcalories.add(((-55.0969 + (0.6309 * 143) + (0.1988 * user.getWeight()) + (0.2017 * userAge))/4.184) * 60 * timeArray.get(6)/3600.0);
+        assertEquals(actualcalories, calories);
+    }
+
+    @Test
+    public void getCaloriesFromActivityFemale() {
+        user.setGender("Female");
+        ArrayList<Double> calories = dataAnalysis.getCaloriesFromActivity(activities.get(1), user);
+        ArrayList<Double> actualcalories = new ArrayList<>();
+        int userAge = Calendar.getInstance().get(Calendar.YEAR) - user.getBirthDate().getYear();
+        ArrayList<Integer> timeArray = dataAnalysis.getTimeFromActivity(activities.get(1));
+        actualcalories.add(((-20.4022 + (0.4472 * 157) + (0.1263 * user.getWeight()) + (0.074 * userAge))/4.184) * 60 * timeArray.get(0)/3600.0);
+        actualcalories.add(((-20.4022 + (0.4472 * 156) + (0.1263 * user.getWeight()) + (0.074 * userAge))/4.184) * 60 * timeArray.get(1)/3600.0);
+        actualcalories.add(((-20.4022 + (0.4472 * 153) + (0.1263 * user.getWeight()) + (0.074 * userAge))/4.184) * 60 * timeArray.get(2)/3600.0);
+        actualcalories.add(((-20.4022 + (0.4472 * 149) + (0.1263 * user.getWeight()) + (0.074 * userAge))/4.184) * 60 * timeArray.get(3)/3600.0);
+        actualcalories.add(((-20.4022 + (0.4472 * 147) + (0.1263 * user.getWeight()) + (0.074 * userAge))/4.184) * 60 * timeArray.get(4)/3600.0);
+        actualcalories.add(((-20.4022 + (0.4472 * 143) + (0.1263 * user.getWeight()) + (0.074 * userAge))/4.184) * 60 * timeArray.get(5)/3600.0);
+        actualcalories.add(((-20.4022 + (0.4472 * 143) + (0.1263 * user.getWeight()) + (0.074 * userAge))/4.184) * 60 * timeArray.get(6)/3600.0);
+        assertEquals(actualcalories, calories);
+    }
+
+    @Test
+    public void getCaloriesFromActivityOther() {
+        user.setGender("Other");
+        ArrayList<Double> calories = dataAnalysis.getCaloriesFromActivity(activities.get(1), user);
+        ArrayList<Double> actualcalories = new ArrayList<>();
+        int userAge = Calendar.getInstance().get(Calendar.YEAR) - user.getBirthDate().getYear();
+        ArrayList<Integer> timeArray = dataAnalysis.getTimeFromActivity(activities.get(1));
+        actualcalories.add(((-37.5 + (0.54 * 157) + (0.155 * user.getWeight()) + (0.138 * userAge))/4.184) * 60 * timeArray.get(0)/3600.0);
+        actualcalories.add(((-37.5 + (0.54 * 156) + (0.155 * user.getWeight()) + (0.138 * userAge))/4.184) * 60 * timeArray.get(1)/3600.0);
+        actualcalories.add(((-37.5 + (0.54 * 153) + (0.155 * user.getWeight()) + (0.138 * userAge))/4.184) * 60 * timeArray.get(2)/3600.0);
+        actualcalories.add(((-37.5 + (0.54 * 149) + (0.155 * user.getWeight()) + (0.138 * userAge))/4.184) * 60 * timeArray.get(3)/3600.0);
+        actualcalories.add(((-37.5 + (0.54 * 147) + (0.155 * user.getWeight()) + (0.138 * userAge))/4.184) * 60 * timeArray.get(4)/3600.0);
+        actualcalories.add(((-37.5 + (0.54 * 143) + (0.155 * user.getWeight()) + (0.138 * userAge))/4.184) * 60 * timeArray.get(5)/3600.0);
+        actualcalories.add(((-37.5 + (0.54 * 143) + (0.155 * user.getWeight()) + (0.138 * userAge))/4.184) * 60 * timeArray.get(6)/3600.0);
         assertEquals(actualcalories, calories);
     }
 
@@ -119,9 +154,24 @@ public class DataAnalysisTest {
     }
 
     @Test
-    public void secondsToTime() {
-        int seconds = 8902;
-        assertEquals(dataAnalysis.secondsToTime(8902), "02:28:22");
+    public void secondsToTimeAddsZeros() {
+        assertEquals(dataAnalysis.secondsToTime(7748), "02:09:08");
+    }
+
+    @Test
+    public void secondsToTimeDoesntAddZeros() {
+        assertEquals(dataAnalysis.secondsToTime(38970), "10:49:30");
+    }
+
+    @Test
+    public void secondsToTimeReturnsNullWhenOver23() {
+        //24 hours in seconds
+        assertEquals(dataAnalysis.secondsToTime(86400), null);
+    }
+
+    @Test
+    public void secondsToTimeReturnsNullWhenNegative() {
+        assertEquals(dataAnalysis.secondsToTime(-1), null);
     }
 
     @Test
