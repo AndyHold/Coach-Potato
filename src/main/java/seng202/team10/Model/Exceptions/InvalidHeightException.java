67ade56a824b4e44ccfd3d2ace8e85a6deddcd5e
Priyesh <psh76@uclive.ac.nz>
2018-09-22 06:52:01 +1200

    Created some more tests, this time for Profile Controller. Also added exception messages to the Exceptions.

--- src/main/java/seng202/team10/Model/Exceptions/InvalidHeightException.java
+++ src/main/java/seng202/team10/Model/Exceptions/InvalidHeightException.java
@@ -1,4 +1,11 @@
 package seng202.team10.Model.Exceptions;
 
 public class InvalidHeightException extends Exception {
+    /**
+     * Constructor method for InvalidHeightException
+     */
+    public InvalidHeightException()
+    {
+        super("Invalid Height");
+    }
 }
