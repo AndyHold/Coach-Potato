c016e4374ad0d43543f1a63545eea83894a9364d
Torben <tkl34@uclive.ac.nz>
2018-09-22 10:48:25 +1200

    Fixed a file path to fix the pipeline

--- src/test/java/seng202/team10/Control/DataAnalysisTest.java
+++ src/test/java/seng202/team10/Control/DataAnalysisTest.java
@@ -32,7 +32,7 @@ public class DataAnalysisTest {
         user.setMaxHeartRate(170);
         DateTime birthday = new DateTime(1996, 12, 04, 0, 0, 0);
         user.setBirthDate(birthday);
-        guiController.uploadDataToUser(user, "/home/cosc/student/tkl34/Desktop/SENG202/SENG202_Project/SENG202_Project/FilesToLoad/testdata.csv");
+        guiController.uploadDataToUser(user, "/FilesToLoad/testdata.csv");
         activities = user.getActivities();
 
     }
